d205f1d8c5db617a0b9898a1ceba446d84818ac6
LongLee <lelongftpshop445@gmail.com>
2026-01-14 14:38:49 +0700

    feat: implements api for apply workkspace code

---
+++ GreenDragonTrading.Application/UseCases/Workspace/Commands/ApplySharedWorkspace/ApplySharedWorkspaceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace GreenDragonTrading.Application.UseCases.Workspace.Commands.ApplySharedWorkspace
+{
+    public class ApplySharedWorkspaceCommandValidator : AbstractValidator<ApplySharedWorkspaceCommand>
+    {
+        public ApplySharedWorkspaceCommandValidator()
+        {
+            RuleFor(x => x.SharedWorkspaceCode)
+                .NotEmpty()
+                .WithMessage("Mã chia sẻ workspace không được để trống.")
+                .Length(8)
+                .WithMessage("Mã chia sẻ workspace phải có 8 ký tự.")
+                .Matches("^[a-zA-Z0-9]+$")
+                .WithMessage("Mã chia sẻ workspace chỉ chứa chữ cái và số.");
+        }
+    }
+}
