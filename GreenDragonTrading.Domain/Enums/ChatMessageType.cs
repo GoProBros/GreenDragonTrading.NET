de89f7d1b8471cb2224e27ad689e3fe985451c3a
LongLee <lelongftpshop445@gmail.com>
2026-03-07 19:58:27 +0700

    feat: impleament chat system table

---
+++ GreenDragonTrading.Domain/Enums/ChatMessageType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDragonTrading.Domain.Enums
+{
+    public enum ChatMessageType : short
+    {
+        [Display(Name = "Text Message")]
+        Text = 0,
+
+        [Display(Name = "Alert Message")]
+        Alert = 1,
+
+        [Display(Name = "File Message")]
+        File = 2,
+
+        [Display(Name = "System Notification")]
+        SystemNotification = 3
+    }
+}
