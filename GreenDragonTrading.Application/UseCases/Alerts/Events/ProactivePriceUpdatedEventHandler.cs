2625df526e7c5b15090c8f3c92ef65f9ad48a0c3
ThinkLee <dinhlethinh137@gmail.com>
2026-04-22 18:28:24 +0700

    update: crud watchlist update cache proactive alert

--- GreenDragonTrading.Application/UseCases/Alerts/Events/ProactivePriceUpdatedEventHandler.cs
+++ GreenDragonTrading.Application/UseCases/Alerts/Events/ProactivePriceUpdatedEventHandler.cs
@@ -412,7 +412,7 @@ namespace GreenDragonTrading.Application.UseCases.Alerts.Events
             => $"alerts:proactive:eval:{ticker}";
 
         private static string WatchListTickerIndexCacheKey()
-            => "alerts:proactive:watchlist-index";
+            => RedisConstants.ProactiveWatchListTickerIndex();
 
         private static string ProactiveUserCooldownKey(Guid userId, string ticker)
             => $"alerts:proactive:cooldown:{userId:N}:{ticker}";
